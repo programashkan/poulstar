73468a76604b9952892f02d63b086b4f21c0a0fb
ashkan <ashkansadeghi2008@gmail.com>
2020-11-19 00:00:03 +0330

    hello

--- term2/example_loop01.py
+++ term2/example_loop01.py
@@ -1,3 +1,4 @@
 lst = ["soad", "ashkan" , "mahan", "amir"]
 for i in lst:
     print("poulstar")
+    number01 int
---
+++ term2/example_test05.py
@@ -0,0 +1,13 @@
+from time import sleep
+number = 0
+max = 7
+while True:
+    sleep(0.5)
+    number += 1
+    if number % 7== 0:
+        print("hope")
+    if number %7  == 0:
+
+
+    print(number)
+
