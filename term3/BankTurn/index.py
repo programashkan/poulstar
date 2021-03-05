8f7a920a2c0b5d0a333c2bb3f3965fde5d88a018
program_ashkan <ashkansadeghi2008@gmail.com>
2021-03-05 16:01:17 +0330

    sdad

--- term3/BankTurn/index.py
+++ term3/BankTurn/index.py
@@ -1,24 +1,68 @@
+
+
+#date
+#time
+#your turn number
+#Queue
+
 from tkinter import *
+import jdatetime
+
+
+# ─── GLOBAL VARIABLE ────────────────────────────────────────────────────────────
+counter = 0
+list_number = []
+
+
+
+# ─── FUNCTION ───────────────────────────────────────────────────────────────────
+
+
+
+def set_number():
+    global counter
+    counter += 1
+    list_number.append(counter)
+    return list_number
+
+
+
+
+
+
+
+# ─── BANK PAGE ──────────────────────────────────────────────────────────────────
 
 bank = Tk()
+bank.geometry("640x480")
+
+Button(bank, text="Oprator 1").grid(row=0, column=0)
+Button(bank, text="Oprator 2").grid(row=0, column=1)
+Button(bank, text="Oprator 3").grid(row=0, column=2)
+
+Button(bank, text="Next Costumer...", command=set_number).grid(row=1, column=1)
 
 
-#
-# ───────────────────────────────────────────────────────────────────── BANK page ─────
-#
-bank.geometry("320x340")
+# ─── RECEPT PAGE ────────────────────────────────────────────────────────────────
 
-Button(bank , text = "click").pack()
 
-Button(bank , text = "click").pack()
 
-Button(bank , text = "click").pack()
-
-#
-# ─────────────────────────────────────────────────────────────────── RECEPT page ─────
-#
 recept = Toplevel()
+recept.geometry("640x480")
+
+Label(recept, text=jdatetime.datetime.now().strftime("%a, %d %b %Y")).grid(row=0, column=0)
+Label(recept, text=jdatetime.datetime.now().strftime("%H:%M:%S")).grid(row=1, column=0)
+
+number = set_number()
+print(number)
+
+# Label(recept, text=str(number)).grid(row=2, column=0)
+
 
 
 
 bank.mainloop()
+
+
+
+
